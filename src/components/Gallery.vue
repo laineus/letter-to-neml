b8678d524f20a7c3f274e1a91c28616f68419283
Laineus <lainen000@gmail.com>
2025-12-19 15:57:34 +0900

    ギャラリー

---
+++ src/components/Gallery.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import { Container, Rectangle, Image } from 'phavuer'
+import CustomText from './CustomText.vue'
+import { computed, ref, type ComputedRef } from 'vue'
+import { state } from '../lib/state'
+import config from '../lib/config'
+import Button from './Button.vue'
+
+defineEmits(['back'])
+
+type EndingData = {
+  title: ComputedRef<string>
+  image: string
+  storyIndex: number
+}
+
+const endings: EndingData[] = [
+  { title: computed(() => '復讐'), image: 'bg/go1', storyIndex: 0 },
+  { title: computed(() => '投獄'), image: 'bg/go2', storyIndex: 1 },
+  { title: computed(() => '昏睡'), image: 'bg/go3', storyIndex: 2 },
+  { title: computed(() => '心中'), image: 'bg/go4', storyIndex: 3 },
+  { title: computed(() => 'ニーナのいない夜'), image: 'bg/ed1', storyIndex: 4 },
+  { title: computed(() => 'ニーナの使命'), image: 'bg/ed2', storyIndex: 5 },
+  { title: computed(() => '二人で過ごす終焉'), image: 'bg/ed3', storyIndex: 6 },
+  { title: computed(() => 'ネムルの見た夢'), image: 'bg/ed4', storyIndex: 7 }
+]
+
+const selectedEnding = ref<EndingData>()
+
+const isUnlocked = (storyIndex: number) => {
+  return state.value.completedEndings.includes(storyIndex)
+}
+
+const handleThumbnailClick = (ending: EndingData) => {
+  if (!isUnlocked(ending.storyIndex)) return
+  selectedEnding.value = ending
+}
+
+const handleCloseFullscreen = () => {
+  selectedEnding.value = undefined
+}
+
+const cols = 4
+const rows = 2
+const thumbWidth = 240
+const thumbHeight = 135
+const rectHeight = thumbHeight + 40
+const padding = 20
+const sumWidth = (cols * thumbWidth) + ((cols - 1) * padding)
+const sumHeight = (rows * rectHeight) + ((rows - 1) * padding)
+</script>
+
+<template>
+  <Container>
+    <!-- サムネイル -->
+    <Container v-if="!selectedEnding" :x="config.WIDTH.half() - sumWidth.half()" :y="config.HEIGHT.half() - sumHeight.half()">
+      <Container
+        v-for="(ending, index) in endings"
+        :key="ending.storyIndex"
+        :x="(index % cols) * (thumbWidth + padding)"
+        :y="Math.floor(index / cols) * (rectHeight + padding)"
+      >
+        <Rectangle
+          :width="thumbWidth"
+          :height="rectHeight"
+          :fillColor="0x000000"
+          :alpha="0.4"
+          :origin="0"
+          @pointerdown="() => handleThumbnailClick(ending)"
+        />
+        <Image
+          v-if="isUnlocked(ending.storyIndex)"
+          :texture="ending.image"
+          :displayWidth="thumbWidth"
+          :displayHeight="thumbHeight"
+          :origin="0"
+        />
+        <Rectangle
+          v-else
+          :width="thumbWidth"
+          :height="thumbHeight"
+          :fillColor="0x000000"
+          :alpha="0.5"
+          :origin="0"
+        />
+        <CustomText
+          :text="isUnlocked(ending.storyIndex) ? ending.title.value : '？？？'"
+          :x="thumbWidth / 2"
+          :y="thumbHeight + 20"
+          :origin="0.5"
+          :style="{ fontSize: 17, color: '#ffffff' }"
+        />
+      </Container>
+      <CustomText text="Gallery" :x="sumWidth.half()" :y="-60" :origin="0.5" :style="{ fontSize: 42 }" />
+      <Button :text="'戻る'" :origin="0.5" :x="sumWidth.half()" :y="sumHeight + 70" @click="$emit('back')" />
+    </Container>
+    <!-- フルスクリーン表示 -->
+    <Image
+      v-if="selectedEnding !== undefined"
+      :texture="selectedEnding.image"
+      :x="1920 / 2"
+      :y="1080 / 2"
+      :origin="0.5"
+      width="1920"
+      height="1080"
+      @pointerdown="handleCloseFullscreen"
+    />
+  </Container>
+</template>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { Container, Rectangle, Image, useScene } from 'phavuer'
+import { Container, Rectangle, Image, useScene, FxBlur } from 'phavuer'
 import config from '../lib/config'
 import CustomText from './CustomText.vue'
 import { computed, onBeforeUnmount, ref } from 'vue'
 import { state } from '../lib/state'
+import Gallery from './Gallery.vue'
 const scene = useScene()
 const bgm = scene.sound.add('bgm/letter-to-neml', { loop: true, volume: state.value.settings.volume })
 bgm.play()
@@ -26,6 +27,7 @@ const menu = [
   {
     label: computed(() => 'ギャラリー'),
     action: () => {
+      type.value = 'gallery'
     }
   },
   {
@@ -36,6 +38,7 @@ const menu = [
   {
     label: computed(() => '終了'),
     action: () => {
+      type.value = 'title'
     }
   },
 ]
@@ -44,16 +47,20 @@ const menu = [
 <template>
   <Container>
     <Rectangle 
-      v-if="type === 'title'"
+      v-show="type === 'title'"
       :width="config.WIDTH" 
       :height="config.HEIGHT" 
       :origin="0"
       :fillColor="0x000000"
       @pointerdown="type = 'menu'"
     />
-    <Image texture="etc/title-bg" :origin="0" />
-    <Image texture="etc/logo-bg" :origin="0.5" :x="config.WIDTH / 2" :y="config.HEIGHT * 0.3" />
-    <Image texture="etc/logo-color" :origin="0.5" :x="config.WIDTH / 2" :y="config.HEIGHT * 0.3" :blendMode="1" />
+    <Image texture="etc/title-bg" :origin="0">
+      <FxBlur :quality="1" :x="4" :y="4" :steps="5" :strength="1.5" v-if="type === 'gallery' || type === 'config'" />
+    </Image>
+    <template v-if="type === 'title' || type === 'menu'">
+      <Image texture="etc/logo-bg" :origin="0.5" :x="config.WIDTH / 2" :y="config.HEIGHT * 0.3" />
+      <Image texture="etc/logo-color" :origin="0.5" :x="config.WIDTH / 2" :y="config.HEIGHT * 0.3" :blendMode="1" />
+    </template>
     <!-- <Image texture="etc/logo" :origin="0.5" :x="config.WIDTH / 2" :y="config.HEIGHT * 0.3" /> -->
     <CustomText
       v-if="type === 'title'"
@@ -65,9 +72,10 @@ const menu = [
     />
     <Container :x="config.WIDTH / 2" :y="config.HEIGHT * 0.62" v-if="type === 'menu'">
       <Container v-for="(item, i) in menu" :key="i" :y="i * 50">
-        <Rectangle :width="360" :height="36" :fillColor="0x000000" :alpha="0.2" :origin="0.5" @pointerdown="item.action" />
+        <Rectangle :width="360" :height="36" :fillColor="0x000000" :alpha="0.4" :origin="0.5" @pointerdown="item.action" />
         <CustomText :text="item.label.value" :origin="0.5" :style="{ fontSize: '17px' }" />
       </Container>
     </Container>
+    <Gallery v-if="type === 'gallery'" @back="type = 'menu'" />
   </Container>
 </template>
