cc357d57c5551be1392a23099a26a98041b6e984
Laineus <lainen000@gmail.com>
2025-12-22 01:53:33 +0900

    v

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 
 <template>
   <Container>
-    <Rectangle :fillColor="0x000000" :strokeColor="0xFFFFFF" :lineWidth="2" :width="size" :height="size" :origin="0" @pointerdown="$emit('click')" />
+    <Rectangle :fillColor="0x000000" :fillAlpha="0.4" :strokeColor="0xFFFFFF" :lineWidth="2" :width="size" :height="size" :origin="0" @pointerdown="$emit('click')" />
     <Image :texture="`etc/${icon}`" :x="size.half()" :y="size.half() + 1" :origin="0.5" :scale="size / 90" />
   </Container>
 </template>
