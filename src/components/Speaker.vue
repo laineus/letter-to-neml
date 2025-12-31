da9553b679b951de30b5e57bb3f99f4f0efa80ca
Laineus <lainen000@gmail.com>
2026-01-01 02:05:31 +0900

    キャラの縮尺調整

--- src/components/Speaker.vue
+++ src/components/Speaker.vue
@@ -3,6 +3,17 @@ import { Container, Image, type Phavuer } from 'phavuer'
 import config from '../lib/config'
 import { computed, ref, watch, type PropType } from 'vue'
 import type { SpeakerConfig } from '../story/types'
+const DEFAULT_SCALE = 0.55
+const charaScale: Record<string, number> = {
+  neml: 0.53,
+  nina: 0.54,
+  aunt: 0.57,
+  trader: 0.57,
+  shopkeeper: 0.57,
+  guard: 0.57,
+  doctor: 0.56,
+  korrin: 0.53
+}
 </script>
 
 <script setup lang="ts">
@@ -23,27 +34,30 @@ watch(realX, (newX, oldX) => {
   }
 })
 const baseImage = computed(() => props.speaker.image.split(':')[0])
+const charaKey = computed(() => baseImage.value.split('/').pop() ?? '')
 const faceImage = computed(() =>  props.speaker.image.split(':')[1] ? props.speaker.image : undefined)
-const SCALE = 0.55
+const scale = computed(() => charaScale[charaKey.value] ?? DEFAULT_SCALE)
 </script>
 
 <template>
   <Container
     :x="initX"
-    :y="config.HEIGHT * SCALE"
-    :scaleX="speaker.facing === 'right' ? SCALE : -SCALE"
-    :scaleY="SCALE"
+    :y="config.HEIGHT + 70"
+    :scaleX="speaker.facing === 'right' ? scale : -scale"
+    :scaleY="scale"
     :tween="tween"
   >
     <Image
       :texture="baseImage"
-      :origin="0.5"
+      :originX="0.5"
+      :originY="1"
       :tint="focus ? 0xFFFFFF : 0x888888"
     />
     <Image
       v-if="faceImage"
       :texture="faceImage"
-      :origin="0.5"
+      :originX="0.5"
+      :originY="1"
       :tint="focus ? 0xFFFFFF : 0x888888"
     />
   </Container>
