14a0daad4a906a372c71af992c58946bca66a7af
Laineus <lainen000@gmail.com>
2026-01-12 02:00:28 +0900

    title logo

--- src/components/TitleScene.vue
+++ src/components/TitleScene.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Container, Rectangle, Scene } from 'phavuer'
+import { Container, Image, Rectangle, Scene, type Phavuer } from 'phavuer'
 import { preloadAssets } from '../lib/preload'
 import Title from './Title.vue'
 import { initGamePad } from '../lib/gamePad'
@@ -8,6 +8,7 @@ import CustomText from './CustomText.vue'
 import config from '../lib/config'
 const progress = ref(0)
 const prepared = ref(false)
+const displayedLogo = ref(false)
 const init = (scene: Phaser.Scene) => {
   initGamePad(scene)
 }
@@ -21,15 +22,31 @@ const preload = (scene: Phaser.Scene) => {
   preloadAssets(scene)
   scene.add.rectangle(0, 0, 10, 10, 0xff0000)
 }
+const logoTween: Phavuer.TweenConfig = {
+  duration: 600,
+  yoyo: true,
+  hold: 900,
+  props: {
+    alpha: {
+      from: 0, to: 1
+    }
+  },
+  onComplete: () => {
+    displayedLogo.value = true
+  }
+}
 </script>
 
 <template>
   <Scene name="TitleScene" :auto-start="true" @init="init" @preload="preload">
-    <Title v-if="prepared" />
+    <template v-if="prepared">
+      <Title v-if="displayedLogo" />
+      <Image v-else texture="etc/logo-laineus" :x="config.WIDTH.half()" :y="config.HEIGHT.half()" :tween="logoTween" />
+    </template>
     <Container :x="config.WIDTH.half()" :y="config.HEIGHT.half()" v-else>
       <CustomText :text="`Loading ${Math.round(progress * 100)}%`" :origin="0.5" :y="-30" />
-      <Rectangle :width="200" :height="4" :fillColor="0x555555" :originY="0" />
-      <Rectangle :width="200" :height="4" :fillColor="0x66bb00" :originX="0" :originY="0" :x="-100" :scaleX="progress" />
+      <Rectangle :width="200" :height="3" :fillColor="0x333333" :originY="0" />
+      <Rectangle :width="200" :height="3" :fillColor="0xffffff" :originX="0" :originY="0" :x="-100" :scaleX="progress" />
     </Container>
   </Scene>
 </template>
